630cff1688ce5f75cbceb07855f16b8527c6e543
vagrant <vagrant@vagrant.vm>
2017-04-16 05:48:45 +0000

    Part 3 of oo

--- object-orientation/assessment.py
+++ object-orientation/assessment.py
@@ -47,47 +47,57 @@ class Student(object):
 
 class Question(object):
     """Class Question of just one question and answer. """
-    question_info = {}
 
     def __init__(self, question, answer):
         """Instantiates with strings question and answer."""
+        self.question_info = {}
         self.question_info["question"] = question
         self.question_info["correct_answer"] = answer
 
     def __repr__(self):
-        return "Question: {}, Answer: {}".format(
+        return "(Question: {}, Answer: {})".format(
             self.question_info["question"], self.question_info["correct_answer"])
 
     def ask_and_evaluate(self):
         """Returns a boolean based on user's answer to the question. """
         user_input = raw_input(self.question_info["question"] + " > ")
         if user_input == self.question_info["correct_answer"]:
-            print True
+            return True
         else:
-            print False
+            return False
 
 
 class Exam(object):
     """Class Exam contains list of questions.
     """
+    # list of Questions in the exam
     questions = []
-    total_num_questions = 0
-    score = 0
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        pass
+        return "{} exam contains {} questions.".format(
+            self.name, len(self.questions))
 
     def add_question(self, question, answer):
+        """ Adds a new question fo the list of questions for exam."""
         new_question = Question(question, answer)
         self.questions.append(new_question)
-        self.total_num_questions += 1
 
     def administer(self):
+        """ Takes exam and returns score."""
+        total_correct = 0
+        total_num_questions = len(self.questions)
 
-    
+        if total_num_questions == 0:
+            return 0
+        else:
+            for q in self.questions:
+                if q.ask_and_evaluate():
+                    total_correct += 1
+            return (float(total_correct) / float(total_num_questions)) * 100
+
 
 class StudentExam(object, Exam):
     pass
